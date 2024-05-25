4b0fda0bd7528ee6138afad1f1a37cff3db4395a
Matias Ignacio Perez Nauto <lit.io30303@gmail.com>
2024-05-24 23:53:55 -0400

    SK ENV.2

--- app/src/main/java/com/lllit/jetpackcompenavigation/screens/FirstScreen.kt
+++ app/src/main/java/com/lllit/jetpackcompenavigation/screens/FirstScreen.kt
@@ -5,7 +5,6 @@ package com.lllit.jetpackcompenavigation.screens
 import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,65 +12,47 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.DarkGray
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.lllit.jetpackcompenavigation.R
 import com.lllit.jetpackcompenavigation.navigation.AppScreens
-import java.nio.file.FileVisitResult
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 
 @Composable
@@ -79,48 +60,11 @@ fun FirstScreen(navController: NavController) {
     Scaffold(
         content = { BodyContent(navController) },
         topBar = { ToolBar() },
-        bottomBar = {
-            MaterialTheme(
-                colorScheme = darkColorScheme()
-            ) {
-                BottomAppBar(
-                    actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Check,
-                                contentDescription = "Bottom Check"
-                            )
-                        }
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Edit,
-                                contentDescription = "Bottom Edit"
-                            )
-                        }
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Person,
-                                contentDescription = "Bottom Person"
-                            )
-                        }
-                    },
-                    floatingActionButton = {
-                        FloatingActionButton(
-                            onClick = { /*TODO*/ },
-                            containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-                        ) {
-                            Icon(imageVector = Icons.Filled.Add, contentDescription = "Icon ADD")
-                        }
-                    }
-                )
-            }
-        },
-        floatingActionButton = { FAB() },
-        floatingActionButtonPosition = FabPosition.End
-    )
+        bottomBar = { BottomBar() },
+        )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToolBar() {
     MaterialTheme(
@@ -265,7 +209,7 @@ fun BodyContent(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.End
     ) {
-        Text(text = "Siguiente", color = Color.White)
+        //Text(text = "Siguiente", color = Color.White)
         Button(onClick = {
             navController.navigate(route = AppScreens.SecondScreen.route)
         }) {
@@ -277,14 +221,45 @@ fun BodyContent(navController: NavController) {
     }
 }
 
-@Preview
+@Composable
+fun BottomBar() {
+    MaterialTheme(
+        colorScheme = darkColorScheme()
+    ) {
+        BottomAppBar(
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Bottom Check"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Edit,
+                        contentDescription = "Bottom Edit"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Bottom Person"
+                    )
+                }
+            },
+            floatingActionButton = { FAB() },
+            //floatingActionButtonPosition = FabPosition.End
+        )
+    }
+}
+
 @Composable
 fun FAB() {
     val context = LocalContext.current
     FloatingActionButton(onClick = {
         Toast.makeText(context, "Toask", Toast.LENGTH_SHORT).show()
     }) {
-        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "DROID")
+        Icon(Icons.Default.Add, contentDescription = "MORE")
     }
 }
 
--- app/src/main/java/com/lllit/jetpackcompenavigation/screens/SecondScreen.kt
+++ app/src/main/java/com/lllit/jetpackcompenavigation/screens/SecondScreen.kt
@@ -1,17 +1,30 @@
 package com.lllit.jetpackcompenavigation.screens
 
 import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
@@ -19,6 +32,7 @@ import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
@@ -28,95 +42,183 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.lllit.jetpackcompenavigation.R
 import com.lllit.jetpackcompenavigation.navigation.AppScreens
+import java.nio.file.FileVisitResult
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 
 @Composable
 fun SecondScreen(navController: NavController) {
-    Scaffold (
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(
-                        "SecondScreen",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
+    Scaffold(
+        content = { SecondBodyContent(navController) },
+        topBar = { ToolBarSecond() },
+        bottomBar = { SecondBottomBar() },
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToolBarSecond() {
+    MaterialTheme(
+        colorScheme = darkColorScheme()
+    ) {
+        CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                //containerColor = MaterialTheme.colorScheme.primaryContainer,
+                //titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text(
+                    "SecondScreen",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Icon Menu"
                     )
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Icon Menu"
-                        )
-                    }
                 }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Check,
-                            contentDescription = "Bottom Check")
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Edit,
-                            contentDescription = "Bottom Edit")
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Person,
-                            contentDescription = "Bottom Person")
-                    }
-                },
-                floatingActionButton = {
-                    FloatingActionButton(
-                        onClick = { /*TODO*/ },
-                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-                    ) {
-                        Icon(imageVector = Icons.Filled.Add, contentDescription = "Icon ADD")
-                    }
-                }
-            )
-        },
-    ){
-        SecondBodyContent(navController)
+            }
+        )
     }
 }
 
 @Composable
-fun SecondBodyContent(navController: NavController){
-    Column (
-        modifier = Modifier.fillMaxSize(),
+fun SecondBodyContent(navController: NavController) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        item {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.image_pag_2),
+                contentDescription = "Imagen Lisergica"
+            )
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.image_pag_2),
+                contentDescription = "Imagen Lisergica"
+            )
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.image_pag_2),
+                contentDescription = "Imagen Lisergica"
+            )
+        }
+
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(11.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = "Veganiando")
+        horizontalAlignment = Alignment.End,
+
+        ) {
+        //Text(text = "Siguiente")
         Button(onClick = {
+            navController.navigate(route = AppScreens.SecondScreen.route)
+            //navController.popBackStack()
+        }) {
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowRight,
+                contentDescription = "Siguiente pagina"
+            )
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(11.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        //Text(text = "Siguiente")
+        Button(onClick = {
+            //navController.navigate(route = AppScreens.SecondScreen.route)
             navController.popBackStack()
         }) {
-            Text(text = "Volver")
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowLeft,
+                contentDescription = "Siguiente pagina"
+            )
         }
     }
 }
 
+@Composable
+fun SecondBottomBar() {
+    MaterialTheme(
+        colorScheme = darkColorScheme()
+    ) {
+        BottomAppBar(
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Bottom Check"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Edit,
+                        contentDescription = "Bottom Edit"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Bottom Person"
+                    )
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                ) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = "Icon ADD")
+                }
+            }
+        )
+    }
+}
 
 
 
